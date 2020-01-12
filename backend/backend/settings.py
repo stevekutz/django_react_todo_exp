8c8e918e01a21fa96a285f52063d7d2252fa6046
stevekutz <stkutz@gmail.com>
2020-01-11 20:57:20 -0600

    updated settings & added Todo model class

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo', # this was added, this registers todo as app Django can recognize
 ]
 
 MIDDLEWARE = [
--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    # these are properties of Todo model
+    title = models.CharField(max_length = 128)   # 120 in tut
+    description = models.TextField()
+    completed = models.BooleanField(default = False)
+
+    def __str__(self):
+        return self.title
