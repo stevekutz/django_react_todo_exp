7a48c7e4082c67da5bfe5656626029adaa0394ea
stevekutz <stkutz@gmail.com>
2020-01-11 21:37:07 -0600

    added rest_framework & corsheaders, updated settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo', # this was added, this registers todo as app Django can recognize
+    'corsheaders', # add this
+    'rest_framework', # add this 
+    'todo' # this was added, this registers todo as app Django can recognize
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',    # add this
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# we whitelist localhost:3000 because that's where frontend will be served
+# added this
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000/'
+)
